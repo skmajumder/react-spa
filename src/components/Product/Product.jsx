1bc1e4ebffceddaaca96e006dc4ffb0f2b2d5a6c
skmajumder <skmajumder.cse@gmail.com>
2023-03-27 09:04:41 +0600

    Show product and cart

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Product.css";
+
+const Product = (props) => {
+  const {
+    id,
+    img: productImg,
+    name: productName,
+    price,
+    seller: manufacturer,
+    ratings,
+  } = props.product;
+  return (
+    <div className="d-flex product">
+      <div className="product-inner">
+        <img src={productImg} className="product-img" alt={productName} />
+        <div className="product-details">
+          <h2 className="product-name">
+            <a href="#">{productName}</a>
+          </h2>
+          <p className="product-price">Price: {price}</p>
+        </div>
+        <div>
+          <p className="manufacturer">
+            <small>Manufacturer : {manufacturer}</small>
+          </p>
+          <p className="ratings">
+            <small>Ratings : {ratings}</small>
+          </p>
+        </div>
+      </div>
+      <button className="product-btn">
+        Add to Cart <span></span>
+      </button>
+    </div>
+  );
+};
+
+export default Product;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
@@ -12,14 +13,27 @@ const Shop = () => {
     fetchProducts();
   }, []);
   return (
-    <section className="section-shop">
+    <section className="section section-shop">
       <div className="container">
         <div className="d-grid shop-grid">
-          <div className="shop-products">
-          
+          <div className="shop-products d-grid shop-products-grid">
+            {products.map((product) => (
+              <Product key={product.id} product={product}></Product>
+            ))}
           </div>
-          <div className="shop-cart">
-            <h4>Shop cart</h4>
+          <div className="d-flex shop-cart">
+            <h4 className="text-center">Order Summary</h4>
+            <div className="d-flex cart-details">
+              <p>Selected Items: 6</p>
+              <p>Total Price: $1140</p>
+              <p>Total Shipping Charge: $5</p>
+              <p>Tax: $114</p>
+              <p className="total-price">Grand Total: $1559</p>
+            </div>
+            <div className="cart-button">
+              <button className="btn-cart">Clear Cart</button>
+              <button className="btn-review">Review Order</button>
+            </div>
           </div>
         </div>
       </div>
