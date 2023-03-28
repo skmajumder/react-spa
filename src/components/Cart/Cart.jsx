0b002f20908e6fd30a9fca6fe7900ab40e20a1d8
skmajumder <skmajumder.cse@gmail.com>
2023-03-28 22:56:46 +0600

    Cart calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,15 +2,26 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = (props) => {
+  const { cart } = props;
+
+  let sum = 0;
+  let shippingCharge = 0;
+  for (const iterator of cart) {
+    sum += iterator.price;
+    shippingCharge += iterator.shipping;
+  }
+  const tax = (sum * 0.02).toFixed(2);
+  const grandTotal = sum + shippingCharge + tax;
+
   return (
     <div className="d-flex shop-cart">
       <h3 className="text-center">Order Summary</h3>
       <div className="d-flex cart-details">
-        <p>Selected Items: {props.cart.length}</p>
-        <p>Total Price: $1140</p>
-        <p>Total Shipping Charge: $5</p>
-        <p>Tax: $114</p>
-        <p className="total-price">Grand Total: $1559</p>
+        <p>Selected Items: {cart.length}</p>
+        <p>Total Price: ${sum}</p>
+        <p>Shipping Charge: ${shippingCharge}</p>
+        <p>Tax: ${tax}</p>
+        <p className="total-price">Grand Total: ${grandTotal}</p>
       </div>
       <div className="cart-button">
         <button className="btn-cart">Clear Cart</button>
